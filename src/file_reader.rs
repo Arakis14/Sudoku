67cd42030a7bf888b83e021bd61dbfc30912b39a
Arakis14 <wiktor.lasota@vp.pl>
2022-07-08 19:12:17 +0200

    File reader test

---
+++ src/file_reader.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+pub fn read_text_file(path: &str) -> String {
+  let file_content = fs::read_to_string(path)
+  .expect("Something went wrong reading the file");
+  println!("{}", file_content);
+  file_content
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+mod file_reader;
 mod map_builder;
 mod sudoku_main;
 use crate::sudoku_main::SudokuRoot;
 
 fn main() {
+    let file_path = "/home/wiktor/Documents/workspace/Sudoku/src/sudoku.txt";
+    file_reader::read_text_file(file_path);
     let v = map_builder::map_builder();
     let sudoku_instance = SudokuRoot {
         map: v,
